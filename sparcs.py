cd122113156b5f78389c3adcfa2e47e19c6e5789
Angela Zhang <angela.zhang.stonybrook.edu>
2024-10-10 21:42:56 -0400

    updates

--- sparcs.py
+++ sparcs.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 url_nassau = 'https://health.data.ny.gov/resource/5dtw-tffi.csv?hospital_county=Nassau&$limit=500000'
 url_suffolk = 'https://health.data.ny.gov/resource/5dtw-tffi.csv?hospital_county=Suffolk&$limit=500000'
